bdd5042aafa1f8295817bff2dc0df479f85ead84
hgarciaalberto <hgarcia.alberto@gmail.com>
2022-06-25 20:31:14 +0100

    TC-1 Create compose project

---
+++ app/src/main/java/com/udemy/compose/twitterclone/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.udemy.compose.twitterclone
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.udemy.compose.twitterclone.ui.theme.TwitterCloneTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TwitterCloneTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    TwitterCloneTheme {
+        Greeting("Android")
+    }
+}
